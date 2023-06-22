f7b31dd8af8575265b1c89e7dabaa03d45b2354d
sushil kumar gupta <sushil@sushils-MacBook-Air.local>
2023-06-22 16:07:45 +0530

    v

--- jenkin1/src/test/java/test1/TestFun.java
+++ jenkin1/src/test/java/test1/TestFun.java
@@ -3,12 +3,14 @@ package test1;
 import org.testng.annotations.Test;
 
 public class TestFun {
+
+@Test
 public void test1()
 {
     System.out.println("hi this from test1");
 }
 
-
+@Test
 public void test2()
 {
     System.out.println("hi this is jenkins1");
